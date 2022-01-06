8361f356ae0e6f27fe85ddc07c5fde5e0e6177ec
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-01-06 22:03:44 +0900

    update

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,21 +1,25 @@
 import React, { useEffect } from 'react'
 // redux
 import { useAppDispatch } from '../../app/hooks'
-import { fetchAsyncMovies } from '../../features/movies/movieSlice'
+import {
+  fetchAsyncMovies,
+  fetchAsyncShows,
+} from '../../features/movies/movieSlice'
 
 // component
-import MovieListing from '../MovieListing/MovieListing'
+// import MovieListing from '../MovieListing/MovieListing'
 
 const Home = () => {
   const dispatch = useAppDispatch()
   useEffect(() => {
     dispatch(fetchAsyncMovies())
+    dispatch(fetchAsyncShows())
   }, [dispatch])
 
   return (
     <div>
       <div className="banner-img" />
-      <MovieListing />
+      {/* <MovieListing /> */}
     </div>
   )
 }
